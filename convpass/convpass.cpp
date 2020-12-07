073042f527bebe0954516172bc5112ccb4b778f3
Anthony William Opipari <topipari@eecs583b.eecs.umich.edu>
2020-12-07 20:16:23 +0000

    Adding loop identification to pass.

--- convpass/convpass.cpp
+++ convpass/convpass.cpp
@@ -5,6 +5,7 @@
 #include "llvm/Support/raw_ostream.h"
 #include "llvm/Analysis/BlockFrequencyInfo.h"
 #include "llvm/Analysis/BranchProbabilityInfo.h"
+#include "llvm/Analysis/LoopInfo.h"
 
 #include "vector"
 
@@ -18,15 +19,49 @@ struct ConvPass : public FunctionPass {
 
   // Specify the list of analysis passes that will be used inside your pass.
   void getAnalysisUsage(AnalysisUsage &AU) const {
-    AU.addRequired<BlockFrequencyInfoWrapperPass>(); // Analysis pass to load block execution count
-    AU.addRequired<BranchProbabilityInfoWrapperPass>(); // Analysis pass to load branch probability
+    //AU.addRequired<BlockFrequencyInfoWrapperPass>(); // Analysis pass to load block execution count
+    //AU.addRequired<BranchProbabilityInfoWrapperPass>(); // Analysis pass to load branch probability
+    AU.addRequired<LoopInfoWrapperPass>(); 
   }
-
+  
+  
   bool runOnFunction(Function &F) override {
-    BranchProbabilityInfo &bpi = getAnalysis<BranchProbabilityInfoWrapperPass>().getBPI(); 
-    BlockFrequencyInfo &bfi = getAnalysis<BlockFrequencyInfoWrapperPass>().getBFI();
+    //BranchProbabilityInfo &bpi = getAnalysis<BranchProbabilityInfoWrapperPass>().getBPI(); 
+    //BlockFrequencyInfo &bfi = getAnalysis<BlockFrequencyInfoWrapperPass>().getBFI();
+    LoopInfo &LI = getAnalysis<LoopInfoWrapperPass>().getLoopInfo();
+    
+    errs() << F.getName() << "\n";
+    int topLevelLoopCount = 0;
+    for(LoopInfo::iterator i = LI.begin(); i != LI.end(); ++i){
+        topLevelLoopCount += 1;
+    }
+    if(topLevelLoopCount!=1){
+        return false;
+    }
 
     
+    
+    std::vector<Loop*> convLoops;
+
+
+    Loop *currLoop = *LI.begin();
+    while(currLoop){
+	convLoops.push_back(currLoop);
+        int numSubLoops = currLoop->getSubLoops().size();
+        if(numSubLoops>1){
+            return false;
+	}
+	else if (numSubLoops==1) {
+            currLoop = currLoop->getSubLoops().front();
+	}
+	else {
+            currLoop = NULL;
+	}
+    }
+
+    errs() << convLoops.size() << "\n";
+    
+    errs() << "\n";
     return false;
   }
 }; // end of struct ConvPass
