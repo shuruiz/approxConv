a00a7e9438ba018f1884daadcfa05f493fff7bc0
Ethan <zhangzhbd@gmail.com>
2020-12-09 14:08:07 -0500

    test 2d

--- test/src/test.c
+++ test/src/test.c
@@ -14,7 +14,7 @@
 
  #include <stdio.h>
 // Global Constant value
-float a=1.4f;
+float[3][3] kernal=[[1.0,0,1.0],[0,1.0,0],[0,0,1.0]];
 
 float Multiply(){
     float b=2.2f;
@@ -23,10 +23,30 @@ float Multiply(){
     return d;
 }
 
+
+// void conv2d(double in[], int inh, int inw, 
+// 	    double k[], double out[]){
+//     for(int ih=0; ih<(inh-KH+1); ih++) {
+//         for(int iw=0; iw<(inw-KW+1); iw++) {
+//             for(int jh=0; jh<5; jh++){
+//                 for(int jw=0; jw<5; jw++) {
+//                     out[(ih*IW)+iw] += in[((ih+jh)*IW)+iw+jw] * k[(jh*KH)+jw];	
+// 		}
+// 	    }
+//         }
+//     }
+// }
+
+
 int main(int argc, char const *argv[])
 {
 
-    printf("%f\n",Multiply());
 
+    float[3][3] target=[[1.0,0,1.0],[0,1.0,0],[0,0,1.0]];
+	float[3][3] out=[[0,0,0],[0,0,0],[0,0,0]];
+    for(int ih=0; ih<3; ih++) {
+        for(int iw=0; iw<3; iw++) {
+            out[ih][iw] += kernel[ih][iw] *target[iw][ih];	
+    printf(out);      
     return 0;
 }
