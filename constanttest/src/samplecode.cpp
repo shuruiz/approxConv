444c9ba925a2a390c50ca8aefe16e2c2a174f8fc
John Fairbanks Straetmans <jstraet@umich.edu>
2020-12-09 07:06:07 +0000

    samplecode modified

---
+++ constanttest/src/samplecode.cpp
@@ -0,0 +1,19 @@
+// samplecode.cpp
+#include "stdio.h"
+#include <iostream>
+
+int main() {
+	
+	const int array[] = { 0, 1, 1, 3, 0, 5 };
+	
+	int temp = 0;
+	for (int i = 0; i < 6; ++i) {
+		int input;
+		std::cin >> input;
+		temp += array[i] * input;	
+	}
+
+	printf("%d\n", temp);
+	
+	return 0;
+}
