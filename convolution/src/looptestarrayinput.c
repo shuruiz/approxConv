a3105576611dcaf5210f10fe75d7b1eb2f5678eb
John Fairbanks Straetmans <jstraet@umich.edu>
2020-12-10 20:59:11 +0000

    Fixed up input looptest file

--- convolution/src/looptestarrayinput.c
+++ convolution/src/looptestarrayinput.c
@@ -1,27 +1,26 @@
 #include <stdio.h>
 
 
-double array[5][5] ={{  1,   1, 0.1, 0.1,   1},
-				{0.1,   1, 0.1, 0.1,    1},
-				{0.1,   1, 0.1, 0.1, 0.01},
-				{0.1, 0.1, 0.1,   1,    1},
-				{  1, 0.1,   1,   1, 0.01}};
+double array[5][5] ={{  0.1,   0.1, 0.1, 0.1,   0.1},
+				{0.1,   0.11, 0.1, 0.1,    0.1},
+				{0.1,   0.11, 0.1, 0.1, 0.01},
+				{0.1, 0.1, 0.1,   0.1,    0.1},
+				{  0.1, 0.1,   0.1,   0.1, 0.01}};
 
 
-void testloops() {
-    int tot = 0;
+void testloops(double* input) {
+    double tot = 0;
     for(int i=0; i<100; i++) {
         for (int j=0; j<100; j++) {
           for(int k=0; k<5; k++){
             for(int l=0; l<5; l++){
-			  int z;
-              scanf("%d", &i);
-			  tot+= (double)z * array[k][l];
+				
+			  tot+= (double)input[k*5 + l] * array[k][l];
 	        }
 	      }
 	    }
     }
-    fprintf(stdout, "%d\n", tot);
+    fprintf(stdout, "%lf\n", tot);
 }
 
 
@@ -29,7 +28,12 @@ void testloops() {
 
 
 int main(){
-    testloops();
+	double input[25];
+	for (int i = 0; i < 25; ++i) {
+    	scanf("%lf", input + i);
+	}
+
+    testloops(input);
     //int tot=0;
     //for(int i=0; i<10; i++) {
     //    //tot+=1;
