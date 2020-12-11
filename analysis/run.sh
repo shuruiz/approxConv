aceb5b529773da9215ea8039202b55c90f55339b
John Fairbanks Straetmans <jstraet@umich.edu>
2020-12-11 22:47:45 +0000

    Fixing conv issue

--- analysis/run.sh
+++ analysis/run.sh
@@ -1,28 +1,29 @@
 #!/bin/bash
 #!/bin/bash
-cp dataconv.c data.c
 
-ROWS=5
-COLS=5
-SIZE=25
+ROWS=10
+COLS=10
+SIZE=100
 
 	for s in $(seq 0 $SIZE); do
-
+		cp dataconv.c data.c
+		
 		SPARSITY=$s
 		NUMSPARSE=0
+		echo $SPARSITY
 		for i in $(seq 1 $ROWS);
 		do
 			for ((j=1;j<=COLS;j++)); do
 				if [ "$NUMSPARSE" -lt "$SPARSITY" ]; then
-    				sed -i s/@$i$j/10/ data.c;
+    				sed -i s/@$i-$j/10/ data.c;
 				else
-					sed -i s/@$i$j/100/ data.c;
+					sed -i s/@$i-$j/100/ data.c;
 				fi
 				NUMSPARSE=$((NUMSPARSE + 1));
 			done
 		done
 
-
+		cat data.c
 
 
 		PATH_MYPASS=~/projects/583project/build/convpass/LLVMCONV.so ### Action Required: Specify the path to your pass ###
@@ -36,4 +37,12 @@ SIZE=25
 		#chmod +x optimized
 
 		#time ./optimized < ../convolution/inputbig.txt
+
+		
+		clang -emit-llvm -S ${BENCH} -o - | sed s/optnone// | opt -load ${PATH_MYPASS} -mem2reg -sccp -sroa -sccp -mem2reg -sroa -mem2reg -die -dse -adce -sccp -O1 > opt.bc 2> /dev/null 
+	
+	
+		clang opt.bc -o optimized
+		time ./optimized < ../convolution/inputbig.txt
+	
 	done
