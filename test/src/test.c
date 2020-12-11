e12372baf119374e725d95d033fdb5f48ebe7988
Ethan <zhangzhbd@gmail.com>
2020-12-10 20:50:36 -0500

    rewrite

--- test/src/test.c
+++ test/src/test.c
@@ -14,13 +14,25 @@
 
  #include <stdio.h>
 // Global Constant value
-float kernel[3]={1.0,0,1.0};
+float kernel[3]={5.0,3,7.0};
 float a=1.4f;
 
  float Add(){
-     float b=2.2f;
-     float c=4.32f;
-     float d= a*c;
+    float b=2.2f;
+    float c=4.32f;
+    float d= a*c;
+
+    float target[3]={1.0,0,1.0};
+    float out[3]={0,0,0};
+    float sum=0.0f;
+
+    for(int ih=0; ih<3; ih++) {
+        printf("%d \n",ih);
+        float x =sum+a;
+        sum = kernel[0] *target[0] + kernel[1] *target[1] +kernel[2] *target[2]; 
+}
+
+
      return d;
  }
 
