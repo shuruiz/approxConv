f14b6faad732e2064dbbdfcd6e23f9de6994034a
John Fairbanks Straetmans <jstraet@umich.edu>
2020-12-12 22:03:15 +0000

    Added sparsity runtime data csv

--- analysis/run.sh
+++ analysis/run.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 #!/bin/bash
 
+# make output data file
+echo  %sparsity,runtime,baseline > sparsityruntime.out
+
 ROWS=10
 COLS=10
 SIZE=100
@@ -10,7 +13,6 @@ SIZE=100
 		
 		SPARSITY=$s
 		NUMSPARSE=0
-		echo $SPARSITY
 		for i in $(seq 1 $ROWS);
 		do
 			for ((j=1;j<=COLS;j++)); do
@@ -33,7 +35,10 @@ SIZE=100
 		clang -emit-llvm -S ${BENCH} -o - | sed s/optnone// | opt -load ${PATH_MYPASS} -convpass -mem2reg -sccp -sroa -sccp -threshpass -mem2reg -sroa -mem2reg -die -dse -adce -sccp -O1 > opt.bc 2> /dev/null 
 
 		clang opt.bc -o optimized
-		time ./optimized < ../convolution/inputbig.txt | grep real
+
+		echo -n "$SPARSITY," >> sparsityruntime.out
+		{ time ./optimized < ../convolution/inputbig.txt > /dev/null; } 2>&1 | grep real | awk '{print $2}' | cut -c 3- | cut -c -5 | xargs echo -n >> sparsityruntime.out
+		
 		#chmod +x optimized
 
 		#time ./optimized < ../convolution/inputbig.txt
@@ -41,9 +46,10 @@ SIZE=100
 		
 		clang -emit-llvm -S ${BENCH} -o - | sed s/optnone// | opt -load ${PATH_MYPASS} -mem2reg -sccp -sroa -sccp -mem2reg -sroa -mem2reg -die -dse -adce -sccp -O1 > opt1.bc 2> /dev/null 
 	
-	
-		#clang opt1.bc -o optimized
+		echo -n , >> sparsityruntime.out
+		clang opt1.bc -o optimized
 		#time ./optimized < ../convolution/inputbig.txt
+		{ time ./optimized < ../convolution/inputbig.txt > /dev/null; } 2>&1 | grep real | awk '{print $2}' | cut -c 3- | cut -c -5 >> sparsityruntime.out
 	
 		#bash ../viz.sh opt
 		#bash ../viz.sh opt1
